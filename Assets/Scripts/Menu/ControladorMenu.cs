47d005f841320fc1c4ff4357df94fa100fb9488c
TheIcySpark <saacmanjarrez@gmail.com>
2020-08-22 06:41:45 -0700

    Menu principal creado

---
+++ Assets/Scripts/Menu/ControladorMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TinyPlanets.Menu
+{
+    public class ControladorMenu : MonoBehaviour
+    {
+        public void Play()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        } 
+
+        public void Quit()
+        {
+            Debug.Log("Aplicacion finalizada");
+            Application.Quit();
+        }
+    }
+
+}
